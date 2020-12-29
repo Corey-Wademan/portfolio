d8c3c33810fff710aa640f9dc4966502c9d60e28
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-12-28 22:44:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 $('.smooth-goto').on('click', function () {
     $('html, body').animate({
         scrollTop: $(this.hash).offset().top + 50
-    }, 1000);
+    }, 1700);
 });
 
+
+
 $(document).ready(function(){
-$('.popup__img').css('visibility', 'visible');        
+    $('.popup__img').css('visibility', 'visible');
+    
+    $('.popup-show').on('click', function () {
+        $('#popup-checkpoint', '.popup-content').addClass('active');
+    });
+    $('.close-btn').on('click', function () {
+        $('#popup-checkpoint', '.popup-content').removeClass('active');
+    });
 })
+
