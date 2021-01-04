bb00c42de6af2a2bc6fd9a67da4bc23ac4f88470
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-01-03 22:52:41 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,61 @@
-// Smooth scroll
-$('.smooth-goto').on('click', function () {
+$( window ).on( "load", function() {
+
+    // Smooth scroll to section
+  $('.smooth-goto').on('click', function () {
     $('html, body').animate({
         scrollTop: $(this.hash).offset().top + 50
     }, 1700);
-});
+  });
 
-// Popup Functionality
-$(".popup-btn").on("click", function() {
+
+  // Checkpoint Popup 
+
+  $(".popup-btn").on("click", function() {
     $(".popup-overlay, .popup-content").addClass("active");
-});
+  });
 
-$(".close-btn, .popup-overlay").on("click", function() {
+  $(".close-btn").on("click", function() {
     $(".popup-overlay, .popup-content").removeClass("active");
-});
+  });
 
 
+  // Resume Popup
 
-// Scroll-To-Top Btn
-var btn = $('.scroll-top');
+  $(".popup-btn-resume").on("click", function() {
+  $(".popup-resume-content, .popup-resume-overlay").addClass("active");
+  });
 
-$(window).scroll(function() {
+  $(".close-btn-resume, .popup-resume-overlay").on("click", function() {
+  $(".popup-resume-content, .popup-resume-overlay").removeClass("active");
+  });
+
+
+  // About Popup
+
+  $(".popup-btn-about").on("click", function() {
+  $(".popup-about-content, .popup-about-overlay").addClass("active");
+  });
+
+  $(".close-btn, .popup-about-overlay").on("click", function() {
+  $(".popup-about-content, .popup-about-overlay").removeClass("active");
+  });
+
+
+  // Scroll-To-Top Btn
+
+  var btn = $('.scroll-top');
+
+  $(window).scroll(function() {
   if ($(window).scrollTop() > 700) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
   }
-});
+  });
 
-btn.on('click', function(e) {
+  btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, 'slow');
-});
+  });
 
-
-
-
-
-
-
-
+})
