afdd55264e5c369d865ce694c1751ae702dd0e6e
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-02-16 14:32:08 -0500

    Added scroll animation behavior, fixed gifs popups

--- index.js
+++ index.js
@@ -34,25 +34,47 @@ $( window ).on( "load", function() {
   });
 
   // Projects Gif Popup
+
+  /* hides cards if gif is active to not show animation bug
+    if ( $('card-btn').data('clicked', true) ) {
+      $('.card').hide()
+    }
+  */  
   $('.gif-hover1, .gif-btn1').click(function() {
     $('#gif1').toggle();
+    $('.scroll-top').toggle('display');
+     if ( $(window).width() > 800 ) {
+      $('.card, .work, .love-it').toggle('opacity')
+     }
   });
 
   $('.gif-hover2, .gif-btn2').click(function() {
     $('#gif2').toggle();
-    console.log('hi')
-  });
+    $('.scroll-top').toggle('display');
+    if ( $(window).width() > 800 ) {
+      $('.card, .work, .love-it').toggle('opacity')
+     }  });
   $('.gif-hover3, .gif-btn3').click(function() {
     $('#gif3').toggle();
-  });
+    $('.scroll-top').toggle('display');
+    if ( $(window).width() > 800 ) {
+      $('.card, .work, .love-it').toggle('opacity')
+     }  });
 
   $('.gif-hover4, .gif-btn4').click(function() {
     $('#gif4').toggle();
+    $('.scroll-top').toggle('display');
+    if ( $(window).width() > 800 ) {
+      $('.card, .work, .love-it').toggle('opacity')
+     }
   });
 
   $('.gif-hover5, .gif-btn5').click(function() {
     $('#gif5').toggle();
-  });
+    $('.scroll-top').toggle('display');
+    if ( $(window).width() < 800 ) {
+      $('.card, .work, .love-it').toggle('opacity')
+     }  });
 
   // Scroll-To-Top Btn
 
@@ -70,5 +92,95 @@ $( window ).on( "load", function() {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, 'slow');
   });
+
+        // FADE IN / FADE OUT SCROLLING ANIMATIONS 
+  $(function() {
+  
+    var html = $('html');
+    // Detections
+    if (!("ontouchstart" in window)) {
+      html.addClass("noTouch");
+    }
+    if ("ontouchstart" in window) {
+      html.addClass("isTouch");
+    }
+    if ("ontouchstart" in window) {
+      html.addClass("isTouch");
+    }
+   
+    if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+      if (navigator.appVersion.indexOf("Trident") === -1) {
+        html.addClass("isEDGE");
+      } else {
+        html.addClass("isIE isIE11");
+      }
+    }
+    if (navigator.appVersion.indexOf("MSIE") !== -1) {
+      html.addClass("isIE");
+    }
+    if (
+      navigator.userAgent.indexOf("Safari") != -1 &&
+      navigator.userAgent.indexOf("Chrome") == -1
+    ) {
+      html.addClass("isSafari");
+    }
+  
+    // On Screen
+  
+    $.fn.isOnScreen = function() {
+      var elementTop = $(this).offset().top,
+        elementBottom = elementTop + $(this).outerHeight(),
+        viewportTop = $(window).scrollTop(),
+        viewportBottom = viewportTop + $(window).height();
+      return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+  
+    function detection() {
+      for (var i = 0; i < items.length; i++) {
+        var el = $(items[i]);
+        if (el.isOnScreen()) {
+          el.addClass("in-view");
+        } else {
+          el.removeClass("in-view");
+        }
+      }
+    }
+  
+    var items = document.querySelectorAll(
+      "*[data-animate-in], *[data-detect-viewport]"
+    ),
+      waiting = false,
+      w = $(window);
+  
+    w.on("resize scroll", function() {
+      if (waiting) {
+        return;
+      }
+      waiting = true;
+      detection();
+  
+      setTimeout(function() {
+        waiting = false;
+      }, 100);
+    });
+  
+    $(document).ready(function() {
+      setTimeout(function() {
+        detection();
+      }, 500);
+  
+      for (var i = 0; i < items.length; i++) {
+        var d = 0,
+          el = $(items[i]);
+        if (items[i].getAttribute("data-animate-in-delay")) {
+          d = items[i].getAttribute("data-animate-in-delay") / 1000 + "s";
+        } else {
+          d = 0;
+        }
+        el.css("transition-delay", d);
+      }
+    });
+  });
+  // END FADE SCROLLING ANIMATIONS
 
 })
